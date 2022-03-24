fc8ebd5bc539ea57f901fff4b51778722e3e63b4
DonAkkardion <kotvasilij1@gmail.com>
2022-03-24 18:19:13 +0200

    First commit

--- FunnyBusiness/FunnyBusiness/settings.py
+++ FunnyBusiness/FunnyBusiness/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!$3*slc9hksg@=8*5ew5b^adr8&sa_&i%+wup3dbivqvsq2255'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-4pw2df6p.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
